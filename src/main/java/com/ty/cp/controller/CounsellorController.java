a54d9035a6f8e5ac8ba0801a96141b2f27df6796
Kashi-S-A <kashappaappannavar@gmail.com>
2025-01-21 20:37:22 +0530

    final save

--- src/main/java/com/ty/cp/controller/CounsellorController.java
+++ src/main/java/com/ty/cp/controller/CounsellorController.java
@@ -3,6 +3,7 @@ package com.ty.cp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.ty.cp.service.CounsellorService;
 
 @RestController
 @RequestMapping("/counsellor")
+@CrossOrigin(origins = "http://localhost:1000")
 public class CounsellorController {
 
 	@Autowired
